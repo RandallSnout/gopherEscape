4844561f5aa000533194103c3d04a7a2ddc371aa
“Randall <pfeiffer.justin.pfeiffer@gmail.com>
2016-12-17 18:37:13 -0800

    Updated Games Controller

--- client/assets/controllers/gamesController.js
+++ client/assets/controllers/gamesController.js
@@ -4,7 +4,7 @@ app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams'
 	var	level = [
 			[4,4,4,5,4,4,4],
 			[2,2,2,1,2,2,2],
-			[2,2,2,1,2,2,2],
+			[2,2,2,3,2,2,2],
 			[2,2,2,1,2,2,2],
 			[2,2,2,1,1,2,2],
 			[2,2,2,2,1,2,2],
@@ -14,10 +14,10 @@ app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams'
 		
 	var	pacman = { x:4, y:7 };
 	var world = level;
-	var score = 0;
+
  	displayWorld = function(){
  		var output = '';
- 		console.log('this is the board');
+ 		console.log('This is the board');
 		for(var i=0; i<world.length; i++){
 			output += "\n<div class='section'>\n"; 
 			for(var j=0; j<world[i].length; j++){
@@ -43,58 +43,64 @@ app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams'
  	};
 
  	displayPacman = function(){
- 			console.log('this is the gopher');
+ 			console.log('This is the gopher');
 			document.getElementById('pacman').style.top = pacman.y*40+"px";
 			document.getElementById('pacman').style.left = pacman.x*40+"px";
 	};
-
-	displayScore = function(){
-			console.log('this is the score');
-			$scope.score = score;
-	};
+	
 
 	displayWorld();
 	displayPacman();
-	displayScore();
 
-	onkeydown = function(e){
+	document.onkeydown = function(e){
 		if(e.keyCode == 37 && world[pacman.y][pacman.x-1] !=2 && world[pacman.y][pacman.x-1] !=4){
 			pacman.x--;
-			document.getElementById("pacman").style.transform = "rotate(-90deg)";
+			document.getElementById("pacman").style.background = "url('assets/images/left.gif')";
+			movePoints();
 		}
 		else if (e.keyCode==39 && world[pacman.y][pacman.x+1] !=2 && world[pacman.y][pacman.x-1] !=4) {
 			pacman.x++;
-			document.getElementById("pacman").style.transform = "rotate(90deg)";
+			document.getElementById("pacman").style.background = "url('assets/images/right.gif')";
+			movePoints();
 		}
 		else if (e.keyCode==38 && world[pacman.y-1][pacman.x] !=2) {
 			pacman.y--;
-			document.getElementById("pacman").style.transform = "rotate(0deg)";
+			document.getElementById("pacman").style.background = "url('assets/images/backward.gif')";
+			movePoints();
 		}
 		else if (e.keyCode==40 && world[pacman.y+1][pacman.x] !=2) {
 			pacman.y++;
-			document.getElementById("pacman").style.transform = "rotate(180deg)";
-		}
-		if(world[pacman.y][pacman.x]==1){
-			world[pacman.y][pacman.x]=0;
-			score+=10;
-			displayWorld();
-			displayScore();
-		}
-		if(world[pacman.y][pacman.x]==3){
-			world[pacman.y][pacman.x]=0;
-			score-=50;
-			displayWorld();
-			displayScore();
-		}
-		if(world[pacman.y][pacman.x]==5){
-			world[pacman.y][pacman.x]=0;
-			score+=300;
-			displayWorld();
-			displayScore();
-			alert('Player 1 is the winner');
+			document.getElementById("pacman").style.background = "url('assets/images/forward.gif')";
+			movePoints();
 		}
 		console.log(e.keyCode);
 		displayPacman();
 	};
+
+	var points = 0;
+	movePoints = function(){
+		if(world[pacman.y][pacman.x]==1){
+			world[pacman.y][pacman.x] = 0;
+			points+=10;
+			displayWorld();
+			console.log('points:'+points);
+		}
+		if(world[pacman.y][pacman.x]==3){
+			world[pacman.y][pacman.x]=0;
+			points-=50;
+			displayWorld();
+			console.log('points:'+points);
+		}
+		if(world[pacman.y][pacman.x]==5){
+			world[pacman.y][pacman.x]=0;
+			points+=300;
+			displayWorld();
+			console.log('points:'+points);
+			alert('Player 1 is the winner with: '+points+' points!');
+		}
+		$scope.score = points;
+		console.log('This is the Score:'+$scope.score);
+	};
+	movePoints();
   
 }]);
