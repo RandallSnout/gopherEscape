bfd61bd6c1473cb78f60c35972bbab6f1ffae2b4
“Randall <pfeiffer.justin.pfeiffer@gmail.com>
2016-12-18 23:38:30 -0800

    Game Board working

--- client/assets/controllers/gamesController.js
+++ client/assets/controllers/gamesController.js
@@ -1,10 +1,10 @@
-app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams','$location', function($scope, userFactory, $sce, $routeParams, $location) {
+app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams','$location', '$timeout','$animate', function($scope, userFactory, $sce, $routeParams, $location, $timeout, $animate) {
  	
 
 	var	level = [
 			[4,4,4,5,4,4,4],
 			[2,2,2,1,2,2,2],
-			[2,2,2,3,2,2,2],
+			[2,2,2,1,2,2,2],
 			[2,2,2,1,2,2,2],
 			[2,2,2,1,1,2,2],
 			[2,2,2,2,1,2,2],
@@ -12,10 +12,12 @@ app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams'
 			[2,2,2,2,0,2,2]
 		];
 		
-	var	pacman = { x:4, y:7 };
+	var	pacman = { x:4, y:7 }; 
 	var world = level;
+	$scope.showPath = true;
+	$scope.shakeBoard = true;
 
- 	displayWorld = function(){
+ 	var displayWorld = function(){
  		var output = '';
  		console.log('This is the board');
 		for(var i=0; i<world.length; i++){
@@ -23,8 +25,19 @@ app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams'
 			for(var j=0; j<world[i].length; j++){
 				if(world[i][j] == 2)
 					output+="<div class='brick'></div>";
-				else if(world[i][j] == 1)
-					output+="<div class='coin'></div>";
+				else if(world[i][j] == 1){
+					if ($scope.showPath == true) {
+						output+="<div class='empty'></div>";
+						$timeout(function() {
+							output+="<div class='coin'></div>"; 
+							$scope.showPath = false; 
+							console.log('timeout complete'); 
+							displayWorld();
+						}, 800);
+					} else {
+						output+="<div class='coin'></div>";
+					};
+				}
 				else if(world[i][j] == 3)
 					output+="<div class='cherry'></div>";
 				else if(world[i][j] == 4)
@@ -35,19 +48,30 @@ app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams'
 					output+="<div class='empty'></div>";
 			}
 			output += "\n</div>";
-		}
+		};
+		if ($scope.shakeBoard == true) {
+						$scope.animate = 'shake';
+						$timeout(function() {
+							$scope.animate = 'shake';
+							$scope.shakeBoard = false; 
+							console.log('timeout complete'); 
+						}, 800);
+					} else {
+						$scope.animate = 'noShake';
+		};
 		$scope.board = output;
 		$scope.showIt = function(){
 			return $sce.trustAsHtml($scope.board);
 		};
+		$scope.$apply();
  	};
 
- 	displayPacman = function(){
+ 	 var displayPacman = function(){
  			console.log('This is the gopher');
 			document.getElementById('pacman').style.top = pacman.y*40+"px";
 			document.getElementById('pacman').style.left = pacman.x*40+"px";
 	};
-	
+
 
 	displayWorld();
 	displayPacman();
@@ -82,24 +106,23 @@ app.controller('gamesController', ['$scope','userFactory','$sce', '$routeParams'
 		if(world[pacman.y][pacman.x]==1){
 			world[pacman.y][pacman.x] = 0;
 			points+=10;
-			displayWorld();
 			console.log('points:'+points);
 		}
 		if(world[pacman.y][pacman.x]==3){
 			world[pacman.y][pacman.x]=0;
-			points-=50;
-			displayWorld();
+			points-=20;
 			console.log('points:'+points);
 		}
 		if(world[pacman.y][pacman.x]==5){
 			world[pacman.y][pacman.x]=0;
 			points+=300;
-			displayWorld();
 			console.log('points:'+points);
 			alert('Player 1 is the winner with: '+points+' points!');
 		}
 		$scope.score = points;
 		console.log('This is the Score:'+$scope.score);
+		displayWorld();
+		$scope.$apply();
 	};
 	movePoints();
   
